f618f8ed076b12f644c48a53e40e701006f2209b
dmjmaxwell <ayomidimejiayoola@gmail.com>
2023-05-19 18:38:28 +0100

    added a new sign up page

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const main = document.getElementsByClassName("main");
+
+const icon = document.querySelectorAll("#icon")
+const para = document.querySelectorAll("#")
+
+
+
+let duration = setTimeout(interval, 3000);
+
+const interval = () =>{
+
+}
